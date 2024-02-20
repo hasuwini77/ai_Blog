25759014a0e6f815e2747b552e88633bdc3219aa
Edwin Andermyr <andermyredwin@gmail.com>
2024-02-20 15:09:14 +0100

    structure CSS beta 1 done

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+import path from "path";
 import express from "express";
 import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const app = express();
 const port = 3000;
-//Activate CSS
-app.use(express.static("public"));
-// Activating body middleware
+
+// Get the directory path of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the public directory and from Boostrap node modules
+app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
+app.use("/css", express.static(path.join(__dirname, "public/css")));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
